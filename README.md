7e53034a00d8808b0d9f50dae1bef10da6a09eb2
ronnyknight45 <55914110+ronnyknight45@users.noreply.github.com>
2020-02-17 10:14:08 +0530

    Added bootstrap to HTML, Badge to Readme and updated code in Hello_World

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaScript_Tutorial
+
+## By Gourab Sarkar
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+
  This repo contains silly code that I used (am using) for learning JavaScript.
--- hello_world.js
+++ hello_world.js
@@ -1,4 +1,27 @@
-var myname = "Gourab";
-console.log(myname);
+alert("Hello, from JavaScript inside the JS file!");
 
+let myMessage = "Feeling kinda awesome...";
+alert(myMessage);
+
+let admin;
+let name;
+name = "Gourab";
+admin = name;
+alert(admin);
+
+let planetName = "Earth";
+let currentUser = admin;
+console.log(planetName);
+console.log(currentUser);
+
+let data = "Gourab Sarkar";
+let dataNumber = 9;
+alert("Hello " + data);
+alert(`Hello ${data}`);
+alert(`Hello ${1+8}`);
+alert(`hello ${"name"}`);
+
+console.log(typeof planetName);
+console.log(typeof(planetName));
+console.log(typeof(dataNumber));
 console.log("2" > 12);
