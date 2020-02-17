14a3a3b1bdd66b502ac29fbe360d43fd53d8e028
ronnyknight45 <55914110+ronnyknight45@users.noreply.github.com>
2020-02-17 10:40:38 +0530

    added Arrow functions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # JavaScript_Tutorial
 
 ## By Gourab Sarkar
+
 [![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
 
  This repo contains silly code that I used (am using) for learning JavaScript.
---
+++ arrow_functions.js
@@ -0,0 +1,10 @@
+function ask(question, yes, no) {
+    if (confirm(question))
+        alert(yes());
+    else
+        alert(no());
+}
+
+ask("Are you having a good day?", 
+    () => ("It gets even better!"), 
+    () => ("Don't worry. Everything will be okay."));
