ffcfb95208f059b5b49efa24f37270849a647970
GOURAB SARKAR <gourab.sarkar@zensar.com>
2020-02-25 11:44:56 +0530

    Added code for challenge 5

--- coding_challenge_5.js
+++ coding_challenge_5.js
@@ -1,6 +1,6 @@
 /*****************************
-* CODING CHALLENGE 5
-****************************/
+ * CODING CHALLENGE 5
+ ****************************/
 
 /*
 Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
@@ -23,3 +23,77 @@ Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the 
 7. Calculate the average tip for each family
 8. Log to the console which family paid the highest tips on average
 */
+
+var johnExpenses = {
+  fullname: "John Ka-boom",
+  tipAmounts: [],
+  paymentsDue: [],
+  billAmounts: [124, 48, 268, 180, 42],
+  tipCalculator: function() {
+    for (i = 0; i < this.billAmounts.length; i++) {
+      if (this.billAmounts[i] < 50)
+        this.tipAmounts.push(0.2 * this.billAmounts[i]);
+      else if (this.billAmounts[i] >= 50 && this.billAmounts[i] <= 200)
+        this.tipAmounts.push(0.15 * this.billAmounts[i]);
+      else this.tipAmounts.push(0.1 * this.billAmounts[i]);
+    }
+  },
+  paymentsCalculator: function() {
+    for (i = 0; i < this.billAmounts.length; i++) {
+      this.paymentsDue.push(this.billAmounts[i] + this.tipAmounts[i]);
+    }
+  }
+};
+
+johnExpenses.tipCalculator();
+johnExpenses.paymentsCalculator();
+console.log(johnExpenses);
+console.log(johnExpenses.paymentsDue);
+
+var markExpenses = {
+  fullname: "Mark Yakuza",
+  tipAmounts: [],
+  paymentsDue: [],
+  billAmounts: [77, 375, 110, 45],
+  tipCalculator: function() {
+    for (i = 0; i < this.billAmounts.length; i++) {
+      if (this.billAmounts[i] < 100)
+        this.tipAmounts.push(0.2 * this.billAmounts[i]);
+      else if (this.billAmounts[i] >= 100 && this.billAmounts[i] <= 300)
+        this.tipAmounts.push(0.1 * this.billAmounts[i]);
+      else this.tipAmounts.push(0.25 * this.billAmounts[i]);
+    }
+  },
+  paymentsCalculator: function() {
+    for (i = 0; i < this.billAmounts.length; i++) {
+      this.paymentsDue.push(this.billAmounts[i] + this.tipAmounts[i]);
+    }
+  }
+};
+
+markExpenses.tipCalculator();
+markExpenses.paymentsCalculator();
+console.log(markExpenses);
+console.log(markExpenses.paymentsDue);
+
+function tipsAverage(tipAmounts) {
+  sumTips = 0;
+  for (let i = 0; i < tipAmounts.length; i++) {
+    sumTips += tipAmounts[i];
+  }
+  return sumTips / tipAmounts.length;
+}
+
+tipsAverage(johnExpenses.tipAmounts) > tipsAverage(markExpenses.tipAmounts)
+  ? console.log("John's tipping average is higher.")
+  : console.log("Mark's tipping average is higher.");
+
+tipsAverage(johnExpenses.tipAmounts) > tipsAverage(markExpenses.tipAmounts)
+  ? console.log(johnExpenses.tipAmounts)
+  : console.log(markExpenses.tipAmounts);
+
+johnExpenses.averageTip = tipsAverage(johnExpenses.tipAmounts);
+markExpenses.averageTip = tipsAverage(markExpenses.tipAmounts);
+
+console.log(johnExpenses);
+console.log(markExpenses);
